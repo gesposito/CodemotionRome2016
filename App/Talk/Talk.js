c1413445b9e325f9fb4804add0aaa506340f6297
Gianluca Esposito <g@esposi.to>
2016-03-13 17:53:36 +0100

    Upgrade to 0.21.0

--- App/Talk/Talk.js
+++ App/Talk/Talk.js
@@ -12,44 +12,29 @@ var isAndroid = Platform.OS === 'android';
 
 var Talk = React.createClass({
   render: function() {
-    if (!isAndroid) {
-      return (
-        <View style={styles.talkContainer}>
-          <WebView
-            style={styles.webView}
-            injectedJavaScript={injectedJavaScript}
-            automaticallyAdjustContentInsets={false}
-            url={this.props.navigator.state.link}
-            startInLoadingState={true}
-          />
-        </View>
-      );
-    }
-
-    var WebViewAndroid = require('react-native-webview-android');
     return (
       <View style={styles.talkContainer}>
-        <WebViewAndroid
+        <WebView
           style={styles.webView}
           injectedJavaScript={injectedJavaScript}
-          url={this.props.navigator.state.link}
-          onNavigationStateChange={this.onNavigationStateChange}
+          automaticallyAdjustContentInsets={false}
+          source={{ uri: this.props.navigator.state.link }}
+          startInLoadingState={true}
         />
       </View>
     );
-
   }
 
 });
 
 var injectedJavaScript = `
-document.getElementsByClassName("detail-talk")[0].style.maxWidth = "none";
-var link = document.createElement( "link" );
-link.href = "http://milan2015.codemotionworld.com/wp-content/themes/event/css/main.css";
-link.type = "text/css";
-link.rel = "stylesheet";
-link.media = "screen,print";
-document.getElementsByTagName( "head" )[0].appendChild( link );
+  document.getElementsByClassName("detail-talk")[0].style.maxWidth = "none";
+  var link = document.createElement( "link" );
+  link.href = "http://milan2015.codemotionworld.com/wp-content/themes/event/css/main.css";
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  link.media = "screen,print";
+  document.getElementsByTagName( "head" )[0].appendChild( link );
 `;
 
 module.exports = Talk;
--- README.md
+++ README.md
@@ -9,7 +9,7 @@ App Store
 </a>
 
 ## How to install
-You need [React Native](http://facebook.github.io/react-native/docs/getting-started.html) either for iOS, Android or both.
+You need [React Native](http://facebook.github.io/react-native/docs/getting-started.html) properly setup either for iOS, Android, or both.
 
 Then, inside this project:
 
@@ -19,12 +19,8 @@ Then, inside this project:
 
 **To run the iOS app:**
 
-- Open `ios/CodemotionAwesomeProject.xcodeproj` and hit run in Xcode.
-- Open `index.ios.js` in your text editor of choice and edit some lines.
-- Hit ⌘-R in your iOS simulator to reload the app and see your change!
+- `react-native run-ios`
 
 **To run the Android app:**
 
 - `react-native run-android`
-- Open `index.android.js` in your text editor of choice and edit some lines.
-- Press the menu button (F2 by default, or ⌘-M in Genymotion) and select *Reload JS* to see your change!
